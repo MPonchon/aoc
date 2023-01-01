86bb4ae32fda6b40e00463480f717a791d63ef48
Marc Ponchon <marc-ponchon@orange.fr>
2023-01-01 11:34:46 +0100

    segment

--- 2022/python/day15/main.py
+++ 2022/python/day15/main.py
@@ -3,8 +3,11 @@
 # https://adventofcode.com/
 #
 
+from functools import total_ordering
+from typing import Set, Tuple
 from icecream import ic
 from collections import namedtuple
+
 Point = namedtuple('Point', ['x', 'y'])
 
 
@@ -85,7 +88,105 @@ def add_reponse(tag, reponse):
 
 """
 
-def man_dist(p1 ,p2):
+
+# TSegment = namedtuple('Segment', ['left', 'right'])
+
+class Segment():
+    """
+    Segment de droite
+    """
+
+    def __init__(self, l, r):
+        self.left = min(l, r)
+        self.right = max(l, r)
+        
+
+    def __contains__(self, x):
+        if isinstance(x, int):
+            return x >= self.left and x <= self.right
+        elif isinstance(x, Segment):
+            return x.left >= self.left and x.right <= self.right
+
+
+    def merge(self, other) -> bool:
+        """
+        Fusionne 2 segment, retourne True si fusion ok, False si echec
+        """
+        if isinstance(other, Segment):
+            if other.left in self or other.right in self:
+                self.right = max(other.right, self.right)
+                self.left = min(other.left, self.left)
+                return True
+
+            return False
+
+        else:
+            raise ValueError ("Not implemented yet !")
+        
+    def __repr__(self) -> str:
+        return f"|_{self.left},{self.right}_|"
+
+@total_ordering
+class CSensor:
+    def __init__(self, coord: Point, beacon: Point) -> None:
+        self.point = coord
+        self.beacon = beacon
+
+        self.man = man_dist(coord, beacon)
+
+    def in_range(self, other: Point) -> bool:
+        dist = man_dist(self.point, other)
+        return dist <= self.man
+
+    def detection_at_y(self, ym: int, minx, maxx) -> Set:
+        detection = set()
+        bornes = self._bornes_at_y(ym)
+        if bornes:
+            for x in range(bornes[0], bornes[1] + 1):
+                if x >= minx and x <= maxx:
+                    detection.add(x)
+        return detection
+
+    def segment_at_y(self, ym: int, minx, maxx) -> Segment|None:
+        seg = None
+        bornes = self._bornes_at_y(ym)
+        xl = bornes[0]
+        if xl < minx:
+            xl = minx
+        xr = bornes[1]
+        if xr > maxx:
+            xr = maxx
+        # ic(bornes, xl, xr, maxx)
+        if bornes:
+            seg = Segment(xl, xr)
+        return seg
+
+
+    def _bornes_at_y(self, ym: int) -> Tuple[int, int]|None:
+        # ic(man)
+        dym = abs(ym - self.point.y)
+        # ic(dym)
+        if self.man < dym:
+            return None
+        dxm = self.man - dym
+        # ic(dxm)
+        return self.point.x - dxm, self.point.x + dxm
+
+    def __eq__(self, other):
+        return ((self.point, self.beacon) ==
+                (other.point, other.beacon))
+
+    def __lt__(self, other):
+        return self.point.x < other.point.x
+
+    def __hash__(self):
+        return hash(self.point.x * self.point.y*self.beacon.x *self.beacon.y)
+
+    def __repr__(self) -> str:
+        return str(self.point.x) +"," +str(self.point.y) 
+
+
+def man_dist(p1, p2):
     return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
 
@@ -97,6 +198,20 @@ def in_range_sensor(ps, p1, dmax=9) -> bool:
 
 def in_range_dx(xs, x1, dmax=1) -> bool:
     return abs(x1 -xs) <= dmax
+
+
+def bornes_range_sensor(ps, bs, ym):
+    dx = abs(ps.x - bs.x)
+    dy = abs(ps.y - bs.y)
+    man = dx + dy
+    # ic(man)
+    dym = abs(ym - ps.y)
+    # ic(dym)
+    if man < dym:
+        return None
+    dxm = man - dym
+    # ic(dxm)
+    return ps.x - dxm, ps.x + dxm
 
 
 def part1(sensors, ym):
@@ -114,6 +229,7 @@ def part1(sensors, ym):
         if in_range_sensor(sensor, (sensor.x, ym), rayon):
             sensors_ranged.add(sensor)
     ic(sensors_ranged)
+
     line_ym = {}
     nb_beacons_on_line = set(
         beacon_d.beacon for beacon_d in 
@@ -123,43 +239,108 @@ def part1(sensors, ym):
 
     nb = len(sensors_ranged)
     n = 0
+    # for sensor in sorted(sensors_ranged):
+    #     # ic(sensor)
+    #     dmax = sensors[sensor].dmax.x + sensors[sensor].dmax.y # dx + dy
+    #     dmax_x = dmax # - dy
+    #     for x in range(sensor.x-dmax_x, sensor.x+dmax_x +1):
+    #         if x not in line_ym and \
+    #             in_range_sensor(sensor, (x, ym), dmax_x):
+    #                 line_ym[x] = 1
+    #     n += 1
+    #     ic (n , nb)
+
     for sensor in sorted(sensors_ranged):
-        # ic(sensor)
+        ic(sensor)
         dmax = sensors[sensor].dmax.x + sensors[sensor].dmax.y # dx + dy
-        dmax_x = dmax # - dy
-        # sensor limited to dmax
-        # ic(sensor.x-dmax_x, sensor.x+dmax_x +1)
-        for x in range(sensor.x-dmax_x, sensor.x+dmax_x +1):
-            # if sensor == (8,7):
-            #     ic(x)
-            if x not in line_ym and \
-                in_range_sensor(sensor, (x, ym), dmax_x):
-                    line_ym[x] = 1
-                    # ic ("in", x)
-
-                    # abs(sensor.x x) + abd(sensor.y - ym) <= dmax
-                    # abs(sensor.x x) <= dmax - abs( sensor.y - ym) 
-
-                    #         dmax = sensors[sensor].dmax.x + sensors[sensor].dmax.y 
-                    #                                         dy = abs(y_b - y_s) 
-
-
+        bornes = bornes_range_sensor(sensor, sensors[sensor].beacon, ym)
+        if bornes:
+            for x in range(bornes[0], bornes[1]+1):
+                if x not in line_ym and \
+                    in_range_sensor(sensor, (x, ym), dmax):
+                        line_ym[x] = 1
         n += 1
         ic (n , nb)
 
-    nb_libre = len(line_ym) - len(nb_beacons_on_line)
+    no_beacon_here = len(line_ym) - len(nb_beacons_on_line)
     # ic(line_ym)
     # ic(nb_beacons_on_line)
     # ic(nb_libre)
-    return nb_libre
+    return no_beacon_here
+
+
+def beacon_here(sensors, ym, max_range):
+    sensors_ranged = set()
+    for sensor, csensor in sensors.items():
+        # rayon = sensors[sensor].dmax.y + sensors[sensor].dmax.x
+        # if in_range_sensor(sensor, (sensor.x, ym), rayon):
+        #     sensors_ranged.add(sensor)
+        if csensor.in_range( Point(sensor.x, ym)):
+            sensors_ranged.add(sensor)
+
+    # line_ym = set(i for i in range(max_range))
+    line_ym = []
+    # ic(line_ym)
+    # ic(sensors_ranged)
+
+    for sensor in sorted(sensors_ranged):
+        # dmax = sensors[sensor].dmax.x + sensors[sensor].dmax.y # dx + dy
+        # bornes = bornes_range_sensor(sensor, sensors[sensor].beacon, ym)
+        csensor =  sensors[sensor]
+        # s = csensor.detection_at_y(ym, 0, max_range)
+        segment = csensor.segment_at_y(ym, 0, max_range)
+        if segment:
+            # merge segments
+            merged = False
+            for seg in line_ym:
+                merged = seg.merge(segment)
+                if merged:
+                    break
+            if not merged:
+                line_ym.append(segment)
+    return line_ym
+
+
+def freq(x):    return 56_000_011 - x * 4_000_000
+# 19 × 131 × 149 × 151
+
+
+def part2(sensors, ym, max_range):
+    print("part2")
+    ic(ym, max_range)
+    beqs = set()
+    line_ym = beacon_here(sensors, 11, max_range)
+
+    # for y in range(11, 12):
+    for y in range(0, max_range+1):
+        line_ym = beacon_here(sensors, y, max_range)
+        # ic(y, line_ym)
+        for seg in line_ym:
+            # ic(seg)
+            for x in range(seg.left, seg.right +1):
+                yfreq = freq(x)
+                if yfreq > max_range or yfreq < 0: continue
+                # ic(y, x, yfreq)
+                beqs.add ((x, yfreq))
+            
+        if y%10 == 0:
+            ic(y, line_ym, beqs)
+    ic(beqs)
+
+    for beq in beqs:
+        x ,y = beq
+        yfreq = freq(x)
+        if int(yfreq) == yfreq:
+            ic(beq)
+            return beq
 
 
 def main():
     reponse1 = 0
     reponse2 = 0
 
-    file_data = "exemple.txt" ;ym = 10
-    file_data = "input.txt" ;     ym = 2000000
+    file_data = "exemple.txt" ;ym = 10      ; max_range = 20
+    file_data = "input.txt" ;  ym = 2000000; max_range = 4000000
 
     with open(file_data, 'r') as f:
         lines = [line.strip("\n") for line in f.readlines()]
@@ -168,6 +349,7 @@ def main():
     # TODO: your code here
     # commun_part
     sensors = {}
+    csensors = {}
     
     Sensor = namedtuple('Sensor', ['beacon', 'dmax'])
     for line in lines:
@@ -184,11 +366,17 @@ def main():
             beacon = Point(x_b, y_b), 
             dmax = Point(dx, dy))
 
+        csensors[Point(x_s, y_s)] = CSensor(Point(x_b, y_b),Point(dx, dy))
+
     # ic(sensors)
     
-    reponse1 = part1(sensors, ym)
-    ic(reponse1)
-    # reponse2 = part2(lines)
+    # reponse1 = part1(sensors, ym)
+    # ic(reponse1)
+
+        # m
+    # 
+
+    reponse2 = part2(csensors, ym, max_range)
     return reponse1, reponse2
 
 
@@ -202,7 +390,7 @@ if __name__ == "__main__":
     add_reponse("reponse part2", reponse2)
 
 # -----------------------------
-# reponse part1: 5878678
-# reponse part2: 0
+# reponse part1: 0
+# reponse part2: (14, 11)
 # memo part1: 5878678
 # memo part2: 0
--- 2022/python/day15/test/test_one copy.py
+++ 2022/python/day15/test/test_one copy.py
@@ -29,13 +29,6 @@ def print_function_name(function):
 
 class TestOne(unittest.TestCase):
 
-    @print_function_name
-    @unittest.skip("reason for skipping")
-    def test_cmp_both_int(self):
-        p1 = 0
-        p2 = 5
-        self.assertEqual( -1 ,  ordered(p1, p2))
-
 
     @print_function_name
     @unittest.skip("reason for skipping")
@@ -46,7 +39,7 @@ class TestOne(unittest.TestCase):
 
 
     @print_function_name
-    # @unittest.skip("reason for skipping")
+    @unittest.skip("reason for skipping")
     def test_in_range_sensor(self):
         p1 = (8, 7)
         self.assertTrue(in_range_sensor(p1, (8, 16) ))
@@ -54,10 +47,37 @@ class TestOne(unittest.TestCase):
 
 
     @print_function_name
-    # @unittest.skip("reason for skipping")
+    @unittest.skip("reason for skipping")
     def test_in_range_sensor_14(self):
         p1 = (12, 14)
         self.assertTrue(in_range_sensor(p1, (12, 10), 4))
         self.assertFalse(in_range_sensor(p1, (11, 10), 4))
 
 
+    @print_function_name
+    @unittest.skip("reason for skipping")
+    def test_bornes_range_sensor(self):
+        p1 = Point(4, 4)
+        b1 = Point(5, 3)
+        # self.assertEqual(None, bornes_range_sensor(p1, b1, 1))
+
+        # self.assertEqual( (4, 4), bornes_range_sensor(p1, b1, 2))
+        # self.assertEqual( (3, 5), bornes_range_sensor(p1, b1, 3))
+        self.assertEqual( (2, 6), bornes_range_sensor(p1, b1, 4))
+        self.assertEqual( (3, 5), bornes_range_sensor(p1, b1, 5))
+        self.assertEqual( (4, 4), bornes_range_sensor(p1, b1, 6))
+        self.assertEqual(None, bornes_range_sensor(p1, b1, 7))
+
+    @print_function_name
+    # @unittest.skip("reason for skipping")
+    def test_possible_freq(self):
+        # 19 × 131 × 149 × 151
+        m = 4_000_000
+        for x in range(0, m):
+            for y in range(0,m):
+                f = freq(x)
+                if int(f) == y:
+                    ic(x,y, f)
+            if x%100==0: 
+                pc =100*x/4_000_000
+                ic (x, pc)
