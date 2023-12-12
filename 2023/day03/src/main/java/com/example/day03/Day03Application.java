b60e6fb7836b9b1800d8a90de28ac1c3b7ad2bb7
Marc <marc-ponchon@orange.fr>
2023-12-12 11:10:35 +0100

    typo

--- 2023/day03/src/main/java/com/example/day03/Day03Application.java
+++ 2023/day03/src/main/java/com/example/day03/Day03Application.java
@@ -6,9 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
-import java.util.Scanner;
-import java.io.File;
-import java.util.ArrayList;
+
 import com.example.day03.utils.Utils;
 
 
@@ -23,16 +21,22 @@ public class Day03Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Demo");
-		List<String> lines = Utils.loafFile("src/main/resources/demo.txt");
+		List<String> lines = Utils.loadFile("src/main/resources/demo.txt");
 		int sum = Grid.sumParts(lines);
 		System.out.println("sum " + sum);
 
 
 		System.out.println("part1");
-		lines = Utils.loafFile("src/main/resources/input.txt");
+		lines = Utils.loadFile("src/main/resources/input.txt");
 		sum = Grid.sumParts(lines);
 		System.out.println("sum " + sum);
 
+		/**
+		 * Demo
+		 * sum 4361
+		 * part1
+		 * sum 544664
+		 */
 	}
 
 }
