0c41d04f708ac274e713d4691f73cb48e118d24a
Marc Ponchon <marc-ponchon@orange.fr>
2023-12-25 17:30:07 +0100

    part2

--- 2023/day07/src/test/java/com/aoc2023/day07/Day07Test.java
+++ 2023/day07/src/test/java/com/aoc2023/day07/Day07Test.java
@@ -3,7 +3,6 @@ package com.aoc2023.day07;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,8 +12,33 @@ class Day07Test {
     @Test
     void sortHands() {
         List<String> hands = new ArrayList<>(List.of("3KA", "2KK"));
-
-        Day07.sortHands(hands);
+        Day07 day07 = new Day07();
+        day07.cardValues = Day07.cardValues1;
+        day07.sortHands(hands, day07.handComparatorP1);
         System.out.println("hands " + hands);
+    }
+
+    @Test
+    void getDemoPart1() {
+        Day07 day07 = new Day07();
+        assertEquals(6440, day07.getDemoPart1());
+    }
+
+    @Test
+    void getPart1() {
+        Day07 day07 = new Day07();
+        assertEquals(250951660, day07.getPart1());
+    }
+
+    @Test
+    void getDemoPart2() {
+        Day07 day07 = new Day07();
+        assertEquals(5905, day07.getDemoPart2());
+    }
+
+    @Test
+    void getPart2() {
+        Day07 day07 = new Day07();
+        assertEquals(251481660, day07.getPart2());
     }
 }
--- 2023/day07/src/test/java/com/aoc2023/day07/HandTypeTest.java
+++ 2023/day07/src/test/java/com/aoc2023/day07/HandTypeTest.java
@@ -2,25 +2,12 @@ package com.aoc2023.day07;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HandTypeTest {
 
-    @Test
-    void isFiveOfAKind() {
-    }
-
-    @Test
-    void nbSameChar() {
-        assertEquals(5, HandType.maxSameChar("AAAAA"));
-        assertEquals(4, HandType.maxSameChar("ZAAAA"));
-        assertEquals(4, HandType.maxSameChar("AAAAZ"));
-        assertEquals(3, HandType.maxSameChar("AAARZ"));
-        assertEquals(3, HandType.maxSameChar("IAAAZ"));
-        assertEquals(3, HandType.maxSameChar("IYAAA"));
-        assertEquals(3, HandType.maxSameChar("IIAAA"));
-        assertEquals(2, HandType.maxSameChar("IIABA"));
-    }
 
     @Test
     void getType() {
@@ -30,6 +17,7 @@ class HandTypeTest {
         assertEquals(HandType.FULL_HOUSE, HandType.getType("23332"));
         assertEquals(HandType.FULL_HOUSE, HandType.getType("11333"));
         assertEquals(HandType.FULL_HOUSE, HandType.getType("333AA"));
+
         assertEquals(HandType.THREE_OAK, HandType.getType("3331A"));
         assertEquals(HandType.THREE_OAK, HandType.getType("2333A"));
         assertEquals(HandType.THREE_OAK, HandType.getType("IO333"));
@@ -41,17 +29,44 @@ class HandTypeTest {
         assertEquals(HandType.THREE_OAK, HandType.getType("32343"));
     }
 
+
     @Test
-    void findTypes() {
-        HandType.findTypes("AAAAA");
-        HandType.findTypes("1AAAA");
-        HandType.findTypes("AAAA1");
-        HandType.findTypes("11AAA");
-        HandType.findTypes("111AA");
-        HandType.findTypes("111OA");
-        HandType.findTypes("11UAA");
-        HandType.findTypes("U11AA");
-        HandType.findTypes("U11KA");
+    void getMap_with_joker_FIVE_OAK() {
+        assertEquals(HandType.FIVE_OAK, HandType.getType("OOOOO", true));
+        assertEquals(HandType.FIVE_OAK, HandType.getType("JOOOO", true));
+        assertEquals(HandType.FIVE_OAK, HandType.getType("JJJJO", true));
+        assertEquals(HandType.FIVE_OAK, HandType.getType("JOJOJ", true));
+        assertEquals(HandType.FIVE_OAK, HandType.getType("OJJJO", true));
+        assertEquals(HandType.FIVE_OAK, HandType.getType("2JJJ2", true));
     }
 
+    @Test
+    void getMap_with_joker_FOUR_OAK() {
+        assertEquals(HandType.FOUR_OAK, HandType.getType("AOJOO", true));
+        assertEquals(HandType.FOUR_OAK, HandType.getType("AJJOO", true));
+        assertEquals(HandType.FOUR_OAK, HandType.getType("AJJOJ", true));
+        assertEquals(HandType.FOUR_OAK, HandType.getType("T55J5", true));
+    }
+
+    @Test
+    void getMap_with_joker_THREE_OAK() {
+        assertEquals(HandType.THREE_OAK, HandType.getType("23J31", true));
+        assertEquals(HandType.THREE_OAK, HandType.getType("AJJ31", true));
+        assertEquals(HandType.THREE_OAK, HandType.getType("AJ31J", true));
+    }
+
+    @Test
+    void getMap_with_joker_FULL_HOUSE() {
+        assertEquals(HandType.FULL_HOUSE, HandType.getType("23J32", true));
+    }
+
+    @Test
+    void getMap_with_joker_TWO_PAIR() {
+        assertEquals(HandType.TWO_PAIR, HandType.getType("2AA32", true));
+    }
+
+    @Test
+    void getMap_with_joker_ONE_PAIR() {
+        assertEquals(HandType.ONE_PAIR, HandType.getType("2345J", true));
+    }
 }
